590f85a83dd0d0c87d4581427a16e0d98d4fa07e
Ellie Essen <ellie.essen@gmail.com>
2017-07-28 11:20:28 -0700

    Scraped entire storage section of the container store
    Made everything a python class
    
    Updated the create_new_dimensions function to create the ’new
    dimensions’ - now works for some of the strange data that came from the
    other sections of the storage that we scraped from the container store.

--- container/spiders/container.py
+++ container/spiders/container.py
@@ -5,19 +5,30 @@ class ContainerSpider(scrapy.Spider):
     name = "container"
 
     start_urls = [
-            'https://www.containerstore.com/s/storage/decorative-bins-baskets/',
-            'https://www.containerstore.com/s/storage/plastic-bins-baskets/'
+            # 'https://www.containerstore.com/s/storage/decorative-bins-baskets/12',
+            # 'https://www.containerstore.com/s/storage/plastic-bins-baskets/'
+            'https://www.containerstore.com/s/storage/'
     ]
 
     def parse(self, response):
+        types = response.xpath('//ul[@class="filter-options three-col-options"]/descendant::a/@href').extract()
+        for t in types:
+            print('--------------------------------------------')
+            print('t: ', t)
+            print('--------------------------------------------')
+            yield response.follow(t, self.parse_type)
 
+    def parse_type(self, response):
         for href in response.css('.product a::attr(href)'):
+            print('--------------------------------------------')
+            print('parse_type href: ', href)
+            print('--------------------------------------------')
             yield response.follow(href, self.parse_container)
 
         # follow pagination links
         for href in response.css('a.right-arrow-button').xpath('@href'):
-            # print('\n ----------------------------------------- \n new page \n' + str(href.extract()) + '\n')
-            yield response.follow(href, self.parse)
+
+            yield response.follow(href, self.parse_type)
 
     def parse_container(self, response):
         dimensions = response.css('.o-block-text--small li::text').extract()
@@ -27,12 +38,16 @@ class ContainerSpider(scrapy.Spider):
         price = response.xpath('//div[@itemprop="price"]/@content').extract()
         if (price==[]):
             price = response.xpath('//meta[@itemprop="price"]/@content').extract()
+        image = response.xpath('//a/@data-retina-modal-image').extract()
+        if (image ==[]):
+            image = [response.xpath('//ul[@class="o-block-list o-block-list--inline"]/@data-image').extract_first()]
 
         yield {
             'url': response.url,
             'title': response.css('h1::text').extract_first().strip(),
             'dimensions': dimensions,#response.css('.o-block-text--small li::text').extract(),
-            'price': price
+            'price': price,
+            'image': image,
         }
 
 
