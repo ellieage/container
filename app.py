40d05cc09039e35f5421d39cd5765ea1dfbe1108
Ellie Essen <ellie.essen@gmail.com>
2017-11-05 12:35:12 -0800

    Input dimensions available

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import container_search
 from flask import Flask, flash, redirect, render_template, request, session, abort, json
 from random import randint
 # import json
@@ -22,6 +23,14 @@ def index():
     SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
     json_url = os.path.join(SITE_ROOT, "", "data_cleaned.json")
     data = json.load(open(json_url))
+    c_url = os.path.join(SITE_ROOT, "", "data_c.json")
+    c = json.load(open(c_url))
+    c_url = os.path.join(SITE_ROOT, "", "data_c.json")
+    c = json.load(open(c_url))
+    dim_url = os.path.join(SITE_ROOT, "", "data_dim.json")
+    dimensions = json.load(open(dim_url))
+
+
 
     randomNumber = randint(0,len(data)-1)
     c = data[randomNumber]
@@ -30,16 +39,38 @@ def index():
     if len(c['dimensions'])>0:
 
         r = randint(0,len(c['dimensions'])-1)
-    
+
     return render_template(
         'test.html',**locals())
 
 @app.route('/', methods=['POST'])
 def my_form_post():
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    json_url = os.path.join(SITE_ROOT, "", "data_cleaned.json")
+    data = json.load(open(json_url))
+    c_url = os.path.join(SITE_ROOT, "", "data_c.json")
+    c = json.load(open(c_url))
+    c_url = os.path.join(SITE_ROOT, "", "data_c.json")
+    c = json.load(open(c_url))
+    dim_url = os.path.join(SITE_ROOT, "", "data_dim.json")
+    dimensions = json.load(open(dim_url))
 
-    text = request.form['text']
-    processed_text = text.upper()
-    return processed_text
+    my_c = container_search.ContainerSearch(data, c, dimensions)
+    l = request.form['text']
+    w = request.form['text2']
+    h = request.form['text3']
+    values=[float(l),float(w),float(h)]
+    pf = my_c.the_perfect_fit_values(values,2.)
+    possibles = pf[3]
+    possibles_text = 'There are '+str(len(possibles))+ ' containers that will fit your space: '+ str(values)+'<br>'
+    # print('')
+    for c in possibles:
+        possibles_text+='<br> <a href='+str(c[0]['url'])+'>'+str(c[0]['title']) + ", '"+str(c[0]['category'])+ "'" +'<br>' +str(c[0]['dimensions'][c[1]])+'<br>'  +'<img src="'+str(c[0]['image'][0])+'/">' +'</a>' #+str(c[0]['price'][c[1]])+'<br>'
+
+
+    # text = request.form['text']
+    # processed_text = text.upper()
+    return possibles_text
 
 if __name__ == "__main__":
     # app.run(host='0.0.0.0', port=80)
---
+++ container_search.py
@@ -0,0 +1,113 @@
+import json
+import os
+
+class ContainerSearch:
+
+
+    # def __init__(self, json_file_name='data_cleaned.json', c_file_name='data_c.json', dim_file_name='data_dim.json'):
+    def __init__(self, data, c, dimensions):
+        """
+        This class is to be used by the website
+        Everything else can be tidied up in the background
+
+        Loads 'data'
+
+        'data' is a list of dictionaries.
+            Each dictionary contains the 'title' (name) of the container, its 'url',
+            a list of the 'dimensions' available, a list of the 'price'(s) in the same order as dimension,
+            and a link to the url of the 'image'.
+            Also a list of 'new dimensions' and category, calculated in container_utils.py
+
+
+        """
+        self.data = data
+        self.c = c
+        self.dimensions = dimensions
+
+
+
+    def the_perfect_fit_ranges(self,ranges):
+
+        """
+        Searches for containers that fit in a given range of dimensions, using the 'dimensions' variable,
+            loaded in during __init__ (a list of lists of dictionaries containing
+            container information).
+        Based on whether 0,1,2, or 3 dimensions are given, a different strategy is used.
+
+        Args:
+            ranges: a list of 6 floats, two for each dimension, providing the lower and upper bound.
+                for example to find a container in the range 6-8.25" x 9-12" x 5-7", the variable would be
+                ranges = [6.,8.25,9.,12.,5.,7.]
+        Returns:
+            possibles: a list of 4 lists of dictionaries of containers that might work.
+                The 4 lists correspond to containers with 0,1,2, or 3 dimensions found.
+        """
+
+        if len(ranges)==6:
+
+            possibles = [[],[],[],[]]
+
+            possibles[0]=self.dimensions[0]
+
+            for d in self.dimensions[1]:
+                if ((ranges[0]<d[0]['new dimensions'][d[1]][0]<ranges[1])
+                    or (ranges[2]<d[0]['new dimensions'][d[1]][0]<ranges[3])
+                    or (ranges[4]<d[0]['new dimensions'][d[1]][0]<ranges[5])):
+
+                    possibles[1].append(d)
+
+            for d in self.dimensions[2]:
+                if ( (ranges[0] < d[0]['new dimensions'][d[1]][0] < ranges[1])  and (ranges[2] < d[0]['new dimensions'][d[1]][1] < ranges[3]) ):
+
+                    possibles[2].append(d)
+
+            for d in self.dimensions[3]:
+                if ((ranges[0]<d[0]['new dimensions'][d[1]][0]<ranges[1]) and (ranges[2]<d[0]['new dimensions'][d[1]][1]<ranges[3])
+                    and (ranges[4]<d[0]['new dimensions'][d[1]][2]<ranges[5])):
+
+                    possibles[3].append(d)
+
+
+            return possibles
+
+
+    def the_perfect_fit_values(self,values,diffmax=2.):
+
+        """
+        Searches for continers that are closest to the given 3 dimensions, using the 'dimensions' variable,
+            created in 'organize_new_dimensions' function (a list of lists of dictionaries containing
+            container information).
+        Based on whether 0,1,2, or 3 dimensions are given, a different strategy is used.
+        Currently only looking at containers with 3 dimensions given:
+            looks for boxes with each dimension within .5", then 1", so on until the given upper bound.
+
+        Args:
+            values: a list of 3 floats, one for each dimension
+            diffmax: an upper bound on the error of the dimensions
+        Returns:
+            possibles: a list of 4 lists of dictionaries of containers that might work.
+                The 4 lists correspond to containers with 0,1,2, or 3 dimensions found.
+        """
+
+        if len(values)==3:
+
+            possibles = [[],[],[],[]]
+
+            possibles[0]=self.dimensions[0]
+
+            diff = 0.5
+
+
+            while diff<=diffmax:
+                for d in self.dimensions[3]:
+
+                    if ((abs(values[0]-d[0]['new dimensions'][d[1]][0])<=diff) and (abs(values[1]-d[0]['new dimensions'][d[1]][1])<=diff)
+                    and (abs(values[2]-d[0]['new dimensions'][d[1]][2])<=diff)):
+
+                        if (d not in possibles[3]):
+
+                            possibles[3].append(d)
+
+                diff+=.5
+
+            return possibles
